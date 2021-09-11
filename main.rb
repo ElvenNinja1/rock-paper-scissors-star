bd573701f29290ccaaaabc864f0a87274d3d732b
ElvenNinja1 <91781980+ElvenNinja1@users.noreply.github.com>
2021-09-11 11:40:29 +0100

    Initial commit

---
+++ main.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+puts 'Welcome to.....'
+puts 'ROCK, PAPER, SCISSORS, STAR!'
+puts 'Choose Your Weapon:'
+player_weapon = gets
+puts 'You chose...'
+puts player_weapon
+computer_weapon = 'rock' # totally random!
+puts 'Computer chose...'
+puts computer_weapon
+print 'Rock smashes '
+puts player_weapon
+puts 'YOU LOSE!!!!!'
+puts 'Computer: "Mwahhahaahah! I win again, Sucker!"'
