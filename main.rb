5c6852110ab3f26241df0d4f731605c3c21ecb02
ElvenNinja1 <91781980+ElvenNinja1@users.noreply.github.com>
2021-09-18 11:22:44 +0100

    feat: randomise computer_weapon

--- main.rb
+++ main.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
-
+WEAPONS = ['rock', 'paper', 'scissors', 'star']
 puts 'Welcome to.....'
 puts 'ROCK, PAPER, SCISSORS, STAR!'
 puts 'Choose Your Weapon:'
 player_weapon = gets
 puts 'You chose...'
 puts player_weapon
-computer_weapon = 'rock' # totally random!
+computer_weapon = WEAPONS.sample # totally random!
 puts 'Computer chose...'
 puts computer_weapon
-print 'Rock smashes '
+print computer_weapon
+print ' destroys '
 puts player_weapon
 puts 'YOU LOSE!!!!!'
 puts 'Computer: "Mwahhahaahah! I win again, Sucker!"'
