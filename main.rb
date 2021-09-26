3fe04c271dcb9039e74aa7142fe59994925c2c39
ElvenNinja1 <91781980+ElvenNinja1@users.noreply.github.com>
2021-09-26 21:03:33 +0100

    feat: implement matchup logic

--- main.rb
+++ main.rb
@@ -1,9 +1,35 @@
 # frozen_string_literal: true
 require 'highline'
-WEAPONS = ['rock', 'paper', 'scissors', 'star']
+WEAPONS = ['rock', 'paper', 'scissors', 'star'].freeze
+MATCHUPS = {
+  'rock' => {
+    'rock' => 'tie',
+    'paper' => 2,
+    'scissors' => 1,
+    'star' => 2
+  },
+  'paper' => {
+    'rock' => 1,
+    'paper' => 'tie',
+    'scissors' => 2,
+    'star' => 2
+  },
+  'scissors' => {
+    'rock' => 2,
+    'paper' => 1,
+    'scissors' => 'tie',
+    'star' => 2
+  },
+  'star' => {
+    'rock' => 1,
+    'paper' => 1,
+    'scissors' => 1,
+    'star' => 'tie'
+  }
+}.freeze
 
-def who_wins (weapon_1, weapon_2)
-  'tie' if weapon_1 == weapon_2
+def who_wins(weapon_1, weapon_2)
+  MATCHUPS[weapon_1][weapon_2]
 end
 
 cli = HighLine.new
@@ -22,7 +48,9 @@ result = who_wins(player_weapon, computer_weapon)
 cli.say result
 if result == 'tie'
   cli.say "It's a tie!!"
-else
+elsif result == 1
+  cli.say 'You Win!!!!'
+elsif result == 2
   cli.say 'YOU LOSE!!!!!'
   cli.say 'Computer: "Mwahhahaahah! I win again, Sucker!"'
 end
